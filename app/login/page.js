0220bc1681add71cb9a9e0d34ae94027cf20d13e
Thomas Auguey <thomas.auguey@hotmail.com>
2026-02-05 15:42:42 +0100

    ajoute le formulaire d'autentification

--- app/login/page.js
+++ app/login/page.js
@@ -1,8 +1,47 @@
-import Image from "next/image";
+'use client'
+
 import styles from "./page.module.css";
 
-export default function Login() {
+const Login = () => {
+  const onSubmit = async (event) => {
+    event.preventDefault()
+ 
+    const formData = new FormData(event.target)
+    
+    const payload = {
+      username: formData.get("username"),
+      password: formData.get("password")
+    };
+
+    //{ "username": "string", "password": "string" }
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/login`, {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(payload)
+    })
+
+    if(response.status != 200)
+    {
+      console.log(response);
+      return;
+    }
+ 
+    // Stock les informations d'identification
+    const data = await response.json()
+    localStorage.setItem("token", data["token"]);
+    localStorage.setItem("userId", data["userId"]);
+  }
+ 
   return (
-    <></>
-  );
+    <form onSubmit={onSubmit}>
+      <label htmlFor="username">Nom d'utilisateur</label>
+      <input type="text" name="username" />
+      <label htmlFor="password">Mot de passe</label>
+      <input type="password" name="password" />
+      <button type="submit">Submit</button>
+    </form>
+  )
 }
+export default Login
