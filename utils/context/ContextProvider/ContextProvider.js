8e5de4750c61e7217fb3613d595a62bc88730b57
Thomas Auguey <thomas.auguey@hotmail.com>
2026-02-06 09:13:40 +0100

    fix l'erreur de référence

--- utils/context/ContextProvider/ContextProvider.js
+++ utils/context/ContextProvider/ContextProvider.js
@@ -4,8 +4,14 @@ import { useEffect, useState } from "react";
 import ContextInstance from "@/utils/context/ContextInstance/ContextInstance"
 
 const ContextProvider = ({ children }) => {
-  const [userToken, setUserToken] = useState(localStorage.getItem("token"))
-  const [userId, setUserId] = useState(localStorage.getItem("userId"))
+  const [userToken, setUserToken] = useState(null)
+  const [userId, setUserId] = useState(null)
+
+  // lecture localStorage côté client uniquement
+  useEffect(() => {
+    setUserToken(localStorage.getItem("token"))
+    setUserId(localStorage.getItem("userId"))
+  }, [])
 
   useEffect(()=>{
     localStorage.setItem("token", userToken);
