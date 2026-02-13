a3c6f56cd2390f022687d4d2f710abbf4ec6ea99
Thomas Auguey <thomas.auguey@hotmail.com>
2026-02-13 13:59:39 +0100

    animation du logo

--- components/Logo/Logo.jsx
+++ components/Logo/Logo.jsx
@@ -10,8 +10,24 @@ const Logo = () => {
   }, [])
 
   return (
-    <div className={styles.logo}>
-      <img src='/logo.png'></img>
+    <div className={styles.container}>
+        <div className={styles.logo}>
+            <div className={styles.load}>
+                <div className={`${styles.bar} ${styles.load__bar1}`}></div>
+                <div className={`${styles.bar} ${styles.load__bar2}`}></div>
+                <div className={`${styles.bar} ${styles.load__bar3}`}></div>
+                <div className={`${styles.bar} ${styles.load__bar4}`}></div>
+                <div className={`${styles.bar} ${styles.load__bar5}`}></div>
+            </div>
+            <div className={styles.load}>
+                <div className={`${styles.bar2} ${styles.load__bar1b}`}></div>
+                <div className={`${styles.bar2} ${styles.load__bar2b}`}></div>
+                <div className={`${styles.bar2} ${styles.load__bar3b}`}></div>
+                <div className={`${styles.bar2} ${styles.load__bar4b}`}></div>
+                <div className={`${styles.bar2} ${styles.load__bar5b}`}></div>
+            </div>
+        </div>
+        <img src='/logo.png'></img>
     </div>
   )
 }
