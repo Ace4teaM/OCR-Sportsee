8c3a140dcd739457c0fd2a1cd6c4385ab1e0b5e0
Thomas Auguey <thomas.auguey@hotmail.com>
2026-02-20 09:57:31 +0100

    vite de fausser le graphique si aucune donnée de dispo

--- components/CardioChart/CardioChart.jsx
+++ components/CardioChart/CardioChart.jsx
@@ -9,14 +9,15 @@ import DateSelector from '@/components/DateSelector/DateSelector'
 import Placeholder from '@/components/Placeholder/Placeholder'
 import {formatDateISO} from '@/utils/functions/format'
 
+// applique une fréquence normal au repos (évite de fausser le graphique si aucune donnée de dispo)
 const emptyDataModel = () => [
-  { day: "Lun", min: NaN, max: NaN, average: NaN },
-  { day: "Mar", min: NaN, max: NaN, average: NaN },
-  { day: "Mer", min: NaN, max: NaN, average: NaN },
-  { day: "Jeu", min: NaN, max: NaN, average: NaN },
-  { day: "Ven", min: NaN, max: NaN, average: NaN },
-  { day: "Sam", min: NaN, max: NaN, average: NaN },
-  { day: "Dim", min: NaN, max: NaN, average: NaN },
+  { day: "Lun", min: 60, max: 100, average: 80 },
+  { day: "Mar", min: 60, max: 100, average: 80 },
+  { day: "Mer", min: 60, max: 100, average: 80 },
+  { day: "Jeu", min: 60, max: 100, average: 80 },
+  { day: "Ven", min: 60, max: 100, average: 80 },
+  { day: "Sam", min: 60, max: 100, average: 80 },
+  { day: "Dim", min: 60, max: 100, average: 80 },
 ];
 
 const CardioChart = ({ initialDate, ...rest }) => {
