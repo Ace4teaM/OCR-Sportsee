060c4084808cd03dc7163cf8ef42a65bb1099ad6
Thomas Auguey <thomas.auguey@hotmail.com>
2026-02-25 10:59:57 +0100

    implémente les appels à Mistral IA

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -2,7 +2,7 @@
   accepte les messages utilisateur et fait les requêtes adaptées à l’API Mistral
 */
 
-import { NextResponse } from "next/server";
+import { fetchWithTimeout } from "@/utils/functions/fetchWithTimeout";
 import Joi from "joi"
 
 const schema = Joi.object({
@@ -10,6 +10,11 @@ const schema = Joi.object({
 })
  
 export async function POST(request) {
+  
+  const timeout = 20000 // 20sec
+  const controller = new AbortController()
+  const id = setTimeout(() => controller.abort(), timeout)
+
   try {
     const body = await request.json()
 
@@ -22,12 +27,56 @@ export async function POST(request) {
       )
     }
 
-    console.log(body)
-    return Response.json({ success: true })
+    const apiKey = process.env.MISTRAL_API_KEY
+    const pathname = "/v1/chat/completions"
+    const proxyURL = new URL(pathname, 'https://api.mistral.ai')
+    const proxyRequest = new Request(proxyURL, {
+      method: "POST",
+      body: JSON.stringify({messages:[
+          {
+            // transmet le message
+            role : "user",
+            content : body.message
+          }
+        ],
+        model:"mistral-large-latest"
+      }),
+      headers:{
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + apiKey,
+      },
+      signal: controller.signal
+    })
+  
+    try {
+      const proxyResponse = await fetch(proxyRequest)
+      clearTimeout(id)
+
+      if(proxyResponse.status != 200)
+        throw new Error("Erreur de format de requête")
+      
+      const proxyBody = await proxyResponse.json()
+
+      const iaMessage = proxyBody.choices[0].message.content
+
+      console.log(iaMessage)
+
+      return Response.json({ success: true, response: iaMessage })
+
+    } catch (reason) {
+      const message =
+        reason instanceof Error ? reason.message : 'Unexpected exception'
+  
+      return new Response(message, { status: 500 })
+    }
   } catch (reason) {
     const message =
       reason instanceof Error ? reason.message : 'Unexpected error'
  
     return new Response(message, { status: 500 })
+  }
+  finally {
+    // s'assure que le time out est annulé
+    clearTimeout(id)
   }
 }
